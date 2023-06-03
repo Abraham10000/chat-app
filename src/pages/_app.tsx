1edc29df4d12df80a9810a28974648f2b83762b7
Abraham10000 <hei.abraham.2@gmail.com>
2023-06-03 11:33:01 +0300

    fix : this is just for verifying that my push work

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,6 +1,8 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
+// This comment is just for verifying that my push work **
+
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
 }
