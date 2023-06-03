68c45682296a8de6030821f8e8fd7e4bb6b93d27
Abraham10000 <hei.abraham.2@gmail.com>
2023-06-03 13:34:11 +0300

    fix : git push work perfectly

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import '@/styles/globals.css'
 import type { AppProps } from 'next/app'
 
-// This comment is just for verifying that my push work **
+// Your push work !! **
 
 export default function App({ Component, pageProps }: AppProps) {
   return <Component {...pageProps} />
